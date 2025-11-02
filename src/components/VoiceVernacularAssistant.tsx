9f3a697481d7e481e98c3e5bf9b2f13043eb52a6
AnshXGrind <anshgarg2512@gmail.com>
2025-11-02 09:02:30 +0530

    5

---
+++ src/components/VoiceVernacularAssistant.tsx
@@ -0,0 +1,8 @@
+// Voice assistant removed. Exporting a no-op stub to keep imports safe.
+import React from "react";
+
+const VoiceVernacularAssistant: React.FC = () => {
+  return null;
+};
+
+export default VoiceVernacularAssistant;
