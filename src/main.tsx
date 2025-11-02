ecf8ae69fdabbbc2e63d8ac97b0aae89e9118a70
AnshXGrind <anshgarg2512@gmail.com>
2025-11-02 09:24:44 +0530

    Fix white screen: Remove duplicate LanguageProvider from main.tsx
    The LanguageProvider was being rendered twice - once in main.tsx and once in App.tsx, causing React context conflicts and white screen. Removed from main.tsx since App.tsx is the correct location for route-level providers.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
-import { LanguageProvider } from "./contexts/LanguageContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <App />
   </ThemeProvider>
 );
