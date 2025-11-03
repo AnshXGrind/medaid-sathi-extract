cd9dcdfb68a5cd253a86bd8a280521b419a9f6cb
AnshXGrind <anshgarg2512@gmail.com>
2025-11-03 23:23:38 +0530

    5

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-3ad5617a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n38lv0gb4t8"
+    "revision": "0.vbu4at4qhi"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
